1e3e4ebe00635c905afd12a22b58e3786ded7968
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2023-02-16 09:21:46 +0100

    Connection a la base de donnée

---
+++ DB_Connect.php
@@ -0,0 +1,16 @@
+
+<?php
+$servername = "localhost"; // Nom du serveur de base de données
+$username = "root"; // Nom d'utilisateur de la base de données
+$password = "root"; // Mot de passe de la base de données
+$dbname = "cityscope_db"; // Nom de la base de données
+
+// Connexion à la base de données
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Vérification de la connexion
+if (!$conn) {
+    die("La connexion a échoué : " . mysqli_connect_error());
+}
+echo "La connexion a réussi avec succès !";
+?>
