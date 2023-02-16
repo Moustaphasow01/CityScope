3ab59c19b2743ed743c68bdd42219cd55f4903fc
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2023-02-16 09:30:38 +0100

    mis a jour du mot de passe root

--- DB_Connect.php
+++ DB_Connect.php
@@ -2,7 +2,7 @@
 <?php
 $servername = "localhost"; // Nom du serveur de base de données
 $username = "root"; // Nom d'utilisateur de la base de données
-$password = "root"; // Mot de passe de la base de données
+$password = ""; // Mot de passe de la base de données
 $dbname = "cityscope_db"; // Nom de la base de données
 
 // Connexion à la base de données
