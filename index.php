72a0e72d957eab9ee032d20e0be63f31a58cd604
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2023-02-17 15:14:40 +0100

    Recuperation des donnée de la bdd sous format json

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// se connecter à la base de données
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cityscope_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// vérifier la connexion
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// exécuter la requête pour récupérer les données de la table vlille_realtime
+$sql = "SELECT * FROM vlille_realtime";
+$result = $conn->query($sql);
+
+// récupérer les données dans un tableau associatif
+$data = array();
+while($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+// renvoyer les données au format JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+
+// fermer la connexion
+$conn->close();
+?>
