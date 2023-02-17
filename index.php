423897021232d17dc502cefedec5158526440768
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2023-02-17 16:37:43 +0100

    Back to v1

--- index.php
+++ index.php
@@ -1,20 +1,33 @@
 <?php
-$username = 'root'; // nom d'utilisateur pour se connecter à la base de données
-$password = ''; // mot de passe pour se connecter à la base de données
+// se connecter à la base de données
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cityscope_db";
 
-try {
-    $db = new PDO("mysql:host=localhost;dbname=cityscope_db", $username, $password); // connexion à la base de données en utilisant PDO
-    $data = array(); // initialisation d'un tableau pour stocker les données récupérées
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    foreach($db->query('SELECT * FROM vlille_realtime') as $row) { // récupération des données de la table "vlille_realtime"
-        $data[] = $row; // ajout de chaque ligne au tableau $data
-    }
-
-    echo json_encode($data); // conversion du tableau $data en une chaîne JSON et affichage du résultat
-    print_r($data);
-
-} catch (PDOException $e) { // en cas d'erreur de connexion, affichage d'un message d'erreur et arrêt du script
-    print "Error: " . $e->getMessage() . "<br />";
-    die;
+// vérifier la connexion
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+// exécuter la requête pour récupérer les données de la table vlille_realtime
+$sql = "SELECT * FROM vlille_realtime";
+$result = $conn->query($sql);
+
+// récupérer les données dans un tableau associatif
+$data = array();
+while($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+print_r($data);
+
+
+// renvoyer les données au format JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+
+// fermer la connexion
+$conn->close();
 ?>
