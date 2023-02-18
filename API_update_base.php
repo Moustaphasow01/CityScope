c0af35e340bc01a900ab3c48a726be309047475f
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2023-02-18 15:21:39 +0100

    Résolution du probleme de array to string pour convertir les donnée de l'api en vue de update la base de donée

--- API_update_base.php
+++ API_update_base.php
@@ -14,25 +14,26 @@ if ($conn->connect_error) {
 // récupérer les données de l'API
 $url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion";
 $data = json_decode(file_get_contents($url), true);
+print_r($data);
+
+
 
 // parcourir les enregistrements
 foreach ($data["records"] as $record) {
   // extraire les données de l'enregistrement
-  $id = $record["fields"]["libelle"];
-  $nom = $record["fields"]["nom"];
-  $adresse = $record["fields"]["adresse"];
-  $commune = $record["fields"]["commune"];
-  $etat = $record["fields"]["etat"];
-  $type = $record["fields"]["type"];
-  $geo = $record["fields"]["geo"];
-  $nbPlacesDispo = $record["fields"]["nbplacesdispo"];
-  $nbVelosDispo = $record["fields"]["nbvelosdispo"];
-  $etatConnexion = $record["fields"]["etatconnexion"];
-  $localisation = $record["fields"]["localisation"];
-  $datemiseajour = $record["fields"]["datemiseajour"];
+  $id = (string) $record["fields"]["libelle"];
+  $nom = (string) $record["fields"]["nom"];
+  $adresse = (string) $record["fields"]["adresse"];
+  $commune = (string) $record["fields"]["commune"];
+  $etat = (string) $record["fields"]["etat"];
+  $type = (string) $record["fields"]["type"];
+  $nbPlacesDispo = (string) $record["fields"]["nbplacesdispo"];
+  $nbVelosDispo = (string) $record["fields"]["nbvelosdispo"];
+  $etatConnexion = (string) $record["fields"]["etatconnexion"];
+  $datemiseajour = (string) $record["fields"]["datemiseajour"];
 
   // insérer les données dans la table vlille_realtime
-  $sql = "INSERT INTO vlille_realtime (ID, nom, adresse, commune, etat, type, geo, nbPlacesDispo, nbVelosDispo, etatConnexion, localisation, datemiseajour) VALUES ('$id', '$nom', '$adresse', '$commune', '$etat', '$type', '$geo', '$nbPlacesDispo', '$nbVelosDispo', '$etatConnexion', '$localisation', '$datemiseajour')";
+  $sql = "INSERT INTO vlille_realtime (ID, nom, adresse, commune, etat, type, nbPlacesDispo, nbVelosDispo, etatConnexion, datemiseajour) VALUES ('$id', '$nom', '$adresse', '$commune', '$etat', '$type', '$nbPlacesDispo', '$nbVelosDispo', '$etatConnexion', '$datemiseajour')";
   $conn->query($sql);
 }
 
