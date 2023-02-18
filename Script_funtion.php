1f65886440b6ff3c318d7c1fc72562529e480f21
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2023-02-18 16:15:44 +0100

    ajout des fonction back ( fonction getVlilleById,fonction getInfosByCommune) et des fonction test

--- Script_funtion.php
+++ Script_funtion.php
@@ -44,4 +44,51 @@ function getStationById($id) {
 }
 
 
+function getInfosByCommune($commune) {
+    // se connecter à la base de données
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cityscope_db";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // vérifier la connexion
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // requête SQL pour récupérer les informations correspondantes à la commune
+    $sql = "SELECT ID, nom, adresse, commune, etat, type, nbPlacesDispo, nbVelosDispo, etatConnexion, datemiseajour FROM vlille_realtime WHERE commune='$commune'";
+    $result = $conn->query($sql);
+
+    // vérifier si la requête a retourné des résultats
+    if ($result->num_rows > 0) {
+        // parcourir les enregistrements et stocker les informations dans un tableau
+        $infos = array();
+        while ($row = $result->fetch_assoc()) {
+            $infos[] = array(
+                "ID" => $row["ID"],
+                "nom" => $row["nom"],
+                "adresse" => $row["adresse"],
+                "commune" => $row["commune"],
+                "etat" => $row["etat"],
+                "type" => $row["type"],
+                "nbPlacesDispo" => $row["nbPlacesDispo"],
+                "nbVelosDispo" => $row["nbVelosDispo"],
+                "etatConnexion" => $row["etatConnexion"],
+                "datemiseajour" => $row["datemiseajour"]
+            );
+        }
+        // fermer la connexion et retourner le tableau d'informations
+        $conn->close();
+        return $infos;
+    } else {
+        // fermer la connexion et retourner une chaîne vide si aucun résultat trouvé
+        $conn->close();
+        return "";
+    }
+}
+
+
+
 ?>
--- test.php
+++ test.php
@@ -1,10 +1,24 @@
 <?php
-require_once 'Script_funtion.php';
+/**
 
-// test de la fonction getVlilleById
+Script pour tester les fonctions du fichier Script_function.php
+*/
+require_once 'Script_funtion.php';
+//----------------------------------------------------------------
+/**
+// Test de la fonction getVlilleById
 $id = '30';
 $vlille = getStationById($id);
 echo "Informations pour l'id $id : \n";
 print_r($vlille);
+//----------------------------------------------------------------
+
+// Test de la fonction getInfosByCommune
+$commune = "Lille";
+$infos = getInfosByCommune($commune);
+echo "Informations pour la commune de $commune : \n";
+print_r($infos);
+//----------------------------------------------------------------
+ */
 
 ?>
